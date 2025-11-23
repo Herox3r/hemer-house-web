2a11d411cdcc3631413719e6c3bc0058ae66adb3
Herox3r <Herox4@outlook.com>
2025-11-23 20:26:26 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-/* Estilos adicionales para animaciones suaves */
-html {
-  scroll-behavior: smooth;
-}
-
-body {
-  background-color: #f9fafb; /* Un fondo gris muy suave por si falla algo */
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
